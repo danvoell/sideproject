7581a18a0fd15186cd310b32336b68ebc6d21065
Dan Voell <danvoell@Dans-MacBook-Pro.local>
2013-01-25 08:54:29 -0600

    created new rails shell

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sideproject::Application.config.secret_token = '4d57bcad67650b90753b1ac646882d280d24f3ea29077a06d8c458b9cee3db449c48253490e38cd208f2eb0e0af8083cd3beb39de0af741094d13025ef5ec4fe'
